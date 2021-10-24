68647d7e9e7c735ba7d49e2c0420c6ba762a02e3
pratyanshupandey <pratyan2001@gmail.com>
2021-10-24 12:07:38 +0530

    zaubacorp link extracted - (integrity contraints unresolved)

---
+++ src/zaubacorp.com/DATA.md
@@ -0,0 +1,14 @@
+## links
+
+Table Headers
+* CIN 
+* Company
+* RoC
+* Status
+* link
+
+CIN as key for dictionary
+* 140581 items in total
+
+* 140250 extracted
+* 301 integrity errors
---
+++ src/zaubacorp.com/links.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+from collections import defaultdict
+import json
+
+links = defaultdict(lambda: {})
+pages = 4687
+total_items = 140581
+items_per_page = 30
+
+
+for page in range(1, pages + 1):
+
+    try:
+        item_count = 0
+        source = requests.get(f"https://www.zaubacorp.com/company-list/roc-RoC-Hyderabad/p-{page}-company.html").text
+        soup = BeautifulSoup(source, "lxml")
+
+        table = soup.find('table', id='table')
+
+        for row in table.find_all('tr'):
+            row_elements = [col.text for col in row.find_all('td')]
+            if row_elements:
+                link = row.find('a')['href']
+
+                if links[row_elements[0]] != {}:
+                    print("INTEGRITY ERROR")
+                    print(row_elements)
+                    print(links[row_elements[0]])
+
+                else:
+                    links[row_elements[0]] = {
+                        "Company": row_elements[1],
+                        "RoC": row_elements[2],
+                        "Status": row_elements[3],
+                        "link": link
+                    }
+                    item_count += 1
+
+        if item_count != items_per_page:
+            print(f"NOT ALL ITEMS EXTRACTED ON PAGE {page}")
+
+    except Exception as e:
+        print("ERROR: ", str(e))
+        print(page)
+
+
+print("Items extracted:", len(links))
+print("All items extracted: ", len(links) == total_items)
+
+print("Dumping Data..")
+file = open("../../data/zaubacorp.com/links.json", "w+")
+json.dump(links, file, indent=4)
+file.close()
+
+print("data dumped in data/zaubacorp.com/links.json")
