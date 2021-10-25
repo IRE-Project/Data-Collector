f642f167626b5c7dbd56cbd324a4ca700da3fd11
pratyanshupandey <pratyan2001@gmail.com>
2021-10-25 14:29:46 +0530

    Comparison with 2015master

--- src/zaubacorp.com/utilities.py
+++ src/zaubacorp.com/utilities.py
@@ -1,4 +1,5 @@
 import json
+from openpyxl import load_workbook
 
 
 def dictlinks_to_list():
@@ -18,4 +19,34 @@ def dictlinks_to_list():
     file.close()
 
 
-dictlinks_to_list()
+def match_old_new():
+    """
+    Checks if all the companies in the old list is there in the new list
+    """
+    wb = load_workbook(filename="../../data/company_master_data_upto_Mar_2015_Telangana.xlsx")
+    sheet_obj = wb.active
+
+    file = open("../../data/zaubacorp.com/links.json", "r")
+    links = json.load(file)
+    file.close()
+
+    cin_list = []
+
+    count = 0
+    for row in range(2, 77042):
+        cin = sheet_obj.cell(row, 1).value
+        try:
+            x = links[cin]
+        except KeyError as e:
+            print(cin)
+            count += 1
+            cin_list.append(cin)
+
+    print("Data Count Not in New Data: ", count)
+
+    file = open("../../data/zaubacorp.com/extra_cin_in_master.json", "w+")
+    json.dump({"CIN": cin_list}, file, indent=4)
+    file.close()
+
+
+match_old_new()
